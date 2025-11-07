478539e22164ecb9dac8fdf5071035cb46b2c76e
Nafee Ahnaf Siraj <nafeeahnafsiraj@gmail.com>
2025-11-07 11:48:26 -0700

    US 01.02.01 Implementation

--- EventLotteryApp/app/build.gradle.kts
+++ EventLotteryApp/app/build.gradle.kts
@@ -1,20 +1,18 @@
 plugins {
-    alias(libs.plugins.android.application)
+    id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.eventlotteryapp"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.eventlotteryapp"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -27,24 +25,24 @@ android {
             )
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    buildFeatures {
-        viewBinding = true
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
 dependencies {
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
-    implementation(libs.annotation)
-    implementation(libs.lifecycle.livedata.ktx)
-    implementation(libs.lifecycle.viewmodel.ktx)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+
+    implementation("com.google.firebase:firebase-auth:22.3.1")
+    implementation("com.google.firebase:firebase-firestore:25.1.1")
+    implementation("com.google.firebase:firebase-storage:21.0.1")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
--- EventLotteryApp/app/src/main/java/com/example/eventlotteryapp/AuthActivity.java
+++ EventLotteryApp/app/src/main/java/com/example/eventlotteryapp/AuthActivity.java
@@ -1,52 +1,36 @@
 package com.example.eventlotteryapp;
 
 import android.os.Bundle;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
-
 import com.example.eventlotteryapp.Authorization.AuthViewPagerAdapter;
 import com.google.android.material.tabs.TabLayout;
-
-import java.util.Objects;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class AuthActivity extends AppCompatActivity {
-    TabLayout tabLayout;
-    ViewPager2 viewPager2;
-    AuthViewPagerAdapter viewPagerAdapter;
+
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager;
+    private AuthViewPagerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
 
         tabLayout = findViewById(R.id.authorization_tabs);
-        viewPager2 = findViewById(R.id.auth_view_pager);
-        viewPagerAdapter = new AuthViewPagerAdapter(this);
-        viewPager2.setAdapter(viewPagerAdapter);
+        viewPager = findViewById(R.id.auth_view_pager);
+        adapter = new AuthViewPagerAdapter(this);
+        viewPager.setAdapter(adapter);
 
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager2.setCurrentItem(tab.getPosition());
-            }
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-            }
-        });
-
-        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-            @Override
-            public void onPageSelected(int position) {
-                super.onPageSelected(position);
-                Objects.requireNonNull(tabLayout.getTabAt(position)).select();
-
-            }
-        });
+        new TabLayoutMediator(tabLayout, viewPager,
+                new TabLayoutMediator.TabConfigurationStrategy() {
+                    @Override
+                    public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                        if (position == 0) tab.setText("Login");
+                        else tab.setText("Sign Up");
+                    }
+                }).attach();
     }
 }
